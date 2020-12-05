ce42142a03b2d239e46a260dcf7a3c0648ca978f
ruslanjan <ruslanjan888@gmail.com>
2020-12-05 19:59:28 +0600

    hotfix

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :proctoring, ProctoringWeb.Endpoint,
-  url: [host: "example.com", port: 5000],
+  url: [host: "proc.tau.moe", port: 5000],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
