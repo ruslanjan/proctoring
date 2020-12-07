76660bb6acc1ce3d4b782bc2c9a0d72b8c752a02
ruslanjan <ruslanjan888@gmail.com>
2020-12-07 13:04:11 +0600

    hotfix

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,7 @@ config :proctoring, ProctoringWeb.Endpoint,
   check_origin: [
     "http://prco.tau.moe",
     "http://localhost.com",
-  ]
+  ],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
