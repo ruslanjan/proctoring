20d9185e772453165a2c88d29e5b15446f00baab
ruslanjan <ruslanjan888@gmail.com>
2020-12-07 13:01:30 +0600

    hotfix

--- config/prod.exs
+++ config/prod.exs
@@ -11,6 +11,10 @@ use Mix.Config
 # before starting your production server.
 config :proctoring, ProctoringWeb.Endpoint,
   url: [host: "proc.tau.moe", port: 5000],
+  check_origin: [
+    "http://prco.tau.moe",
+    "http://localhost.com",
+  ]
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
